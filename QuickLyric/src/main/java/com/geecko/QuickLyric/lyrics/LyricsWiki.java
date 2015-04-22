19398294399c4488fc49a2be53b05d0ba4eb3d02
geecko <H.guillaume@gmail.com>
2015-04-22 03:45:44 +0200

    Implement search on LyricsWiki

--- QuickLyric/src/main/java/com/geecko/QuickLyric/lyrics/LyricsWiki.java
+++ QuickLyric/src/main/java/com/geecko/QuickLyric/lyrics/LyricsWiki.java
@@ -27,46 +27,60 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.parser.Parser;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
+import java.text.Normalizer;
+import java.util.ArrayList;
 
+import static com.geecko.QuickLyric.lyrics.Lyrics.ERROR;
+import static com.geecko.QuickLyric.lyrics.Lyrics.NEGATIVE_RESULT;
+import static com.geecko.QuickLyric.lyrics.Lyrics.NO_RESULT;
+import static com.geecko.QuickLyric.lyrics.Lyrics.POSITIVE_RESULT;
+import static com.geecko.QuickLyric.lyrics.Lyrics.SEARCH_ITEM;
 import static com.geecko.QuickLyric.utils.Net.getUrlAsString;
 
 public class LyricsWiki {
 
     @Reflection
     public static final String domain = "lyrics.wikia.com";
-    private static final String baseUrl = "http://lyrics.wikia.com/api.php?action=lyrics&fmt=json&func=getSong&artist=%1s&song=%1s";
+    private static final String baseUrl =
+            "http://lyrics.wikia.com/api.php?action=lyrics&fmt=json&func=getSong&artist=%1s&song=%1s";
+    private static final String baseAPIUrl =
+            "http://lyrics.wikia.com/wikia.php?controller=LyricsApi&method=getSong&artist=%1s&song=%2s";
+    private static final String baseSearchUrl =
+            "http://lyrics.wikia.com/Special:Search?search=%s&fulltext=Search";
 
     @Reflection
-    public static Lyrics fromMetaData(String artist, String title) {
-        if ((artist == null) || (title == null))
-            return new Lyrics(Lyrics.ERROR);
-        String encodedArtist;
-        String encodedSong;
-        String url;
-        String originalArtist = artist;
-        String originalTitle = title;
+    public static ArrayList<Lyrics> search(String query) {
+        ArrayList<Lyrics> results = new ArrayList<>();
+        query = Normalizer.normalize(query, Normalizer.Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
         try {
-            encodedArtist = URLEncoder.encode(artist, "UTF-8");
-            encodedSong = URLEncoder.encode(title, "UTF-8");
-            JSONObject json = new JSONObject(getUrlAsString(new URL(
-                    String.format(baseUrl, encodedArtist, encodedSong))).replace("song = ", ""));
-            url = URLDecoder.decode(json.getString("url"), "UTF-8");
-            artist = json.getString("artist");
-            title = json.getString("song");
-        } catch (JSONException | IOException e) {
+            URL queryURL = new URL(String.format(baseSearchUrl, URLEncoder.encode(query, "UTF-8")));
+            Document searchpage = Jsoup.connect(queryURL.toExternalForm()).get();
+            Elements searchResults = searchpage.getElementsByClass("Results");
+            if (searchResults.size() >= 1) {
+                searchResults = searchResults.get(0).getElementsByClass("result");
+                for (Element searchResult : searchResults) {
+                    String[] tags = searchResult.getElementsByTag("h1").text().split(":");
+                    if (tags.length != 2) continue;
+                    String url = searchResult.getElementsByTag("a").attr("href");
+                    Lyrics lyrics = new Lyrics(SEARCH_ITEM);
+                    lyrics.setArtist(tags[0]);
+                    lyrics.setTitle(tags[1]);
+                    lyrics.setURL(url);
+                    lyrics.setSource(domain);
+                    results.add(lyrics);
+                }
+            }
+        } catch (IOException e) {
             e.printStackTrace();
-            return new Lyrics(Lyrics.ERROR);
         }
-        Lyrics lyrics = fromURL(url, artist, title);
-        lyrics.setOriginalArtist(originalArtist);
-        lyrics.setOriginalTitle(originalTitle);
-        return lyrics;
+        return results;
     }
 
     public static Lyrics fromURL(String url, String artist, String song) {
