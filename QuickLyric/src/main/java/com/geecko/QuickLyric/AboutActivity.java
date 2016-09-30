bcf5b73cbc7c0a6a909d1723e5fe8f774892f770
geecko <H.guillaume@gmail.com>
2016-09-30 15:35:55 +0200

    Fix disappearing resource

--- QuickLyric/src/main/java/com/geecko/QuickLyric/AboutActivity.java
+++ QuickLyric/src/main/java/com/geecko/QuickLyric/AboutActivity.java
@@ -132,7 +132,7 @@ public class AboutActivity extends AppCompatActivity {
         linearLayout.addView(aboutView);
         setContentView(linearLayout);
 
-        final Drawable upArrow = getResources().getDrawable(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+        final Drawable upArrow = getResources().getDrawable(R.drawable.abc_ic_ab_back_material);
         upArrow.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
 
         setSupportActionBar(toolbar);
