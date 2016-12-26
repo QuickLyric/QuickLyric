4b4ac531f1f4d7d742d140c7ad66e95b4e903b13
geecko <H.guillaume@gmail.com>
2016-12-26 17:42:48 +0100

    Bump versionCode and update Database version Number

--- QuickLyric/src/main/java/com/geecko/QuickLyric/utils/DatabaseHelper.java
+++ QuickLyric/src/main/java/com/geecko/QuickLyric/utils/DatabaseHelper.java
@@ -32,7 +32,7 @@ import java.util.List;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 3;
+    private static final int DATABASE_VERSION = 4;
     private static final String DATABASE_NAME = "QuickLyric";
     public static final String TABLE_NAME = "lyrics";
     private static final String KEY_ARTIST = "artist";
@@ -67,10 +67,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        if (oldVersion < 3 && getColumnsCount(db) <= 6) {
-            db.execSQL("ALTER TABLE " + TABLE_NAME + "\n ADD " + KEY_ORIGINAL_ARTIST + " TINYTEXT;");
-            db.execSQL("ALTER TABLE " + TABLE_NAME + "\n ADD " + KEY_ORIGINAL_TRACK + " TINYTEXT;");
-            db.execSQL("ALTER TABLE " + TABLE_NAME + "\n ADD " + KEY_LRC + " BIT;");
+        if (oldVersion < 4) {
+            db.execSQL("DELETE FROM "+TABLE_NAME);
         }
     }
 
